7f5e7767d583837f466d94151a1434d301dac3a2
djaniAntova <djaniantova@gmail.com>
2023-05-28 23:28:28 +0300

    Generate token on login

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,18 +1,24 @@
-const User = require('../models/User');
 
-exports.getUserByUsername =  (username) => User.findOne({ username })
+const User = require('../models/User');
+const config = require('../config');
+const jwt = require('../lib/jsonwebtoken')
+
+exports.getUserByUsername = (username) => User.findOne({ username })
 // this is async function, but by returning no need for async-await, and being short we write it in one line, no need for return
-   
-exports.register = (username, password) =>  User.create({ username, password });
+
+exports.register = (username, password) => User.create({ username, password });
 
 exports.login = async (username, password) => {
     const user = await this.getUserByUsername(username);
 
     const isValid = await user.validatePassword(password)
 
-    if(!user || !isValid) {
+    if (!user || !isValid) {
         throw 'Invalid username or password'
     }
-    return user; 
+    const payload = { username: user.username };
+    const token = await jwt.sign(payload, config.SECRET, { expiresIn: '2h' });
+
+    return token;
 
 }
