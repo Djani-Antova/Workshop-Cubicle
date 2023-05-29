e83635e7538cd8f2db79645ed3a2710148db9586
djaniAntova <djaniantova@gmail.com>
2023-05-29 19:40:15 +0300

    Add auth controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,23 +1,24 @@
 const router = require('express').Router();
 
-const authService = require ('../services/authService')
+const authService = require('../services/authService')
 
 router.get('/login', (req, res) => {
     res.render('auth/login')
 });
 
 router.post('/login', async (req, res) => {
-     const { username, password} = req.body;
+    const { username, password } = req.body;
 
-     try {
-         const token = await authService.login(username, password);
-         console.log(token);
-        
-     } catch (err) {
+    try {
+        const token = await authService.login(username, password);
+
+        res.cookie('auth', token, {httpOnly: true}); 
+
+    } catch (err) {
         console.log(err);
-        return redirect('/')
-     }
-     res.redirect('/');
+       
+    }
+    res.redirect('/');
 })
 
 router.get('/register', (req, res) => {
@@ -27,13 +28,13 @@ router.get('/register', (req, res) => {
 router.post('/register', async (req, res) => {
     const { username, password, repeatPassword } = req.body;
 
-    if(password !== repeatPassword) {
+    if (password !== repeatPassword) {
         return res.redirect('/404');
     }
 
     const existingUser = await authService.getUserByUsername(username);
 
-    if(existingUser) {
+    if (existingUser) {
         return res.redirect('/404');
     }
 
